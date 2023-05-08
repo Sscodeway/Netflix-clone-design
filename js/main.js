9def0b6d70f42c4def2821c6e478d6c52bc5bc36
Sscodeway <ss.nickname00@gmail.com>
2023-05-08 20:45:01 +0300

    edt

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//select tab content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // add border
+    this.classList.add('tab-border')
+    //grab content
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    //add show class
+    tabContentItem.classList.add('Show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+
+//tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
